6a1e75a383d1cafa1446f82ce22bdd54f5ea0be6
michelemilazzo <michele.milazzo@live.com>
2026-01-25 16:12:16 +0100

    Prepare india_compliance for MMOS marketplace

---
+++ hooks.py
@@ -0,0 +1,9 @@
+
+app_name = "india_compliance"
+app_title = "India Compliance"
+app_publisher = "MMOS Marketplace"
+app_description = "India Compliance"
+app_icon = "octicon octicon-star"
+app_color = "#f48024"
+app_email = "developers@frappe.io"
+app_license = "MIT"
